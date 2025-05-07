6a1cb839982dd519667e5cda7861630a7afd21fe
gustavoking <gustavorostirollax@gmail.com>
2025-05-06 23:08:31 -0300

    removing comments

--- src/app/[slug]/menu/components/finish-order-dialog.tsx
+++ src/app/[slug]/menu/components/finish-order-dialog.tsx
@@ -31,8 +31,6 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 
-// import { createOrder } from "../actions/create-order";
-//import { createStripeCheckout } from "../actions/create-stripe-checkout";
 import { CartContext } from "../contexts/cart";
 import { isValidCpf } from "../helpers/cpf";
 
@@ -79,27 +77,6 @@ const FinishOrderDialog = ({ open, onOpenChange }: FinishOrderDialogProps) => {
         "consumptionMethod",
       ) as ConsumptionMethod;
 
-      // const order = await createOrder({
-      //   consumptionMethod,
-      //   customerCpf: data.cpf,
-      //   customerName: data.name,
-      //   products,
-      //   slug,
-      // });
-      // const { sessionId } = await createStripeCheckout({
-      //   products,
-      //   orderId: order.id,
-      //   slug,
-      //   consumptionMethod,
-      //   cpf: data.cpf,
-      // });
-      // if (!process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY) return;
-      // const stripe = await loadStripe(
-      //   process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY,
-      // );
-      // stripe?.redirectToCheckout({
-      //   sessionId: sessionId,
-      // });
     } catch (error) {
       console.error(error);
     } finally {
